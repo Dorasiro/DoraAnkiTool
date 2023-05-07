8b2f748f94a14fbb42be1ab4f461728f3b99a1c4
Dorashiro <dorasiro1@live.jp>
2023-05-07 16:55:17 +0900

    [Update]暫定UI完成

--- DoraAnkiTool/GUI/MainForm.Designer.cs
+++ DoraAnkiTool/GUI/MainForm.Designer.cs
@@ -28,33 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.questionLabel = new System.Windows.Forms.Label();
+            this.answer1Label = new System.Windows.Forms.Label();
+            this.answer2Label = new System.Windows.Forms.Label();
+            this.answer3Label = new System.Windows.Forms.Label();
+            this.answer4Label = new System.Windows.Forms.Label();
+            this.nextButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // questionLabel
             // 
-            this.button1.Location = new System.Drawing.Point(364, 90);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(56, 72);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.questionLabel.AutoSize = true;
+            this.questionLabel.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.questionLabel.Location = new System.Drawing.Point(30, 19);
+            this.questionLabel.Name = "questionLabel";
+            this.questionLabel.Size = new System.Drawing.Size(63, 25);
+            this.questionLabel.TabIndex = 0;
+            this.questionLabel.Text = "label1";
+            // 
+            // answer1Label
+            // 
+            this.answer1Label.AutoSize = true;
+            this.answer1Label.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.answer1Label.Location = new System.Drawing.Point(30, 98);
+            this.answer1Label.Name = "answer1Label";
+            this.answer1Label.Size = new System.Drawing.Size(52, 21);
+            this.answer1Label.TabIndex = 5;
+            this.answer1Label.Text = "label1";
+            this.answer1Label.Click += new System.EventHandler(this.answer1Label_Click);
+            // 
+            // answer2Label
+            // 
+            this.answer2Label.AutoSize = true;
+            this.answer2Label.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.answer2Label.Location = new System.Drawing.Point(30, 159);
+            this.answer2Label.Name = "answer2Label";
+            this.answer2Label.Size = new System.Drawing.Size(52, 21);
+            this.answer2Label.TabIndex = 6;
+            this.answer2Label.Text = "label1";
+            this.answer2Label.Click += new System.EventHandler(this.answer2Label_Click);
+            // 
+            // answer3Label
+            // 
+            this.answer3Label.AutoSize = true;
+            this.answer3Label.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.answer3Label.Location = new System.Drawing.Point(30, 221);
+            this.answer3Label.Name = "answer3Label";
+            this.answer3Label.Size = new System.Drawing.Size(52, 21);
+            this.answer3Label.TabIndex = 7;
+            this.answer3Label.Text = "label1";
+            this.answer3Label.Click += new System.EventHandler(this.answer3Label_Click);
+            // 
+            // answer4Label
+            // 
+            this.answer4Label.AutoSize = true;
+            this.answer4Label.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.answer4Label.Location = new System.Drawing.Point(30, 279);
+            this.answer4Label.Name = "answer4Label";
+            this.answer4Label.Size = new System.Drawing.Size(52, 21);
+            this.answer4Label.TabIndex = 8;
+            this.answer4Label.Text = "label1";
+            this.answer4Label.Click += new System.EventHandler(this.answer4Label_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(30, 333);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(74, 32);
+            this.nextButton.TabIndex = 9;
+            this.nextButton.Text = "次に進む";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Visible = false;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.answer4Label);
+            this.Controls.Add(this.answer3Label);
+            this.Controls.Add(this.answer2Label);
+            this.Controls.Add(this.answer1Label);
+            this.Controls.Add(this.questionLabel);
             this.Name = "MainForm";
             this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private Button button1;
+        private Label questionLabel;
+        private Label answer1Label;
+        private Label answer2Label;
+        private Label answer3Label;
+        private Label answer4Label;
+        private Button nextButton;
     }
 }
--- DoraAnkiTool/GUI/MainForm.cs
+++ DoraAnkiTool/GUI/MainForm.cs
@@ -13,25 +13,159 @@ namespace DoraAnkiTool.GUI
 {
     public partial class MainForm : Form
     {
+        private AnkiToolMain ankiToolMain = new AnkiToolMain();
+        private Dictionary<Label, ThemeDictionaly> dict = new Dictionary<Label, ThemeDictionaly>();
+        private ThemeDictionaly[]? nowQuestions;
+
         public MainForm()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void GetQuestion()
         {
-            var a = new AnkiToolMain();
-            var l = a.GetAllThemeDictionalies();
-            foreach(var elem in l)
+            nowQuestions = ankiToolMain.Questions();
+            if(nowQuestions == null)
             {
-                Debug.WriteLine("単語：" + elem.Word + "　　　意味：" + elem.Mean);
+                questionLabel.Text = "全問題終わり。";
+                answer1Label.Visible = false;
+                answer2Label.Visible= false;
+                answer3Label.Visible= false;
+                answer4Label.Visible= false;
+                return;
             }
-            var r = a.Questions();
-            r = a.Questions();
-            r = a.Questions();
-            r = a.Questions();
-            r = a.Questions();
-            r = a.Questions();
+
+            // 問題文を表示
+            questionLabel.Text = nowQuestions[0].Word;
+
+            // 回答を表示
+            var rnd = new Random();
+            var rndInt = rnd.Next(0, nowQuestions.Length);
+            switch(rndInt)
+            {
+                case 0:
+                    answer1Label.Text = nowQuestions[0].Mean;
+                    answer2Label.Text = nowQuestions[1].Mean;
+                    answer3Label.Text = nowQuestions[2].Mean;
+                    answer4Label.Text = nowQuestions[3].Mean;
+
+                    dict[answer1Label] = nowQuestions[0];
+                    dict[answer2Label] = nowQuestions[1];
+                    dict[answer3Label] = nowQuestions[2];
+                    dict[answer4Label] = nowQuestions[3];
+
+                    break;
+
+                case 1:
+                    answer1Label.Text = nowQuestions[1].Mean;
+                    answer2Label.Text = nowQuestions[0].Mean;
+                    answer3Label.Text = nowQuestions[2].Mean;
+                    answer4Label.Text = nowQuestions[3].Mean;
+
+                    dict[answer1Label] = nowQuestions[1];
+                    dict[answer2Label] = nowQuestions[0];
+                    dict[answer3Label] = nowQuestions[2];
+                    dict[answer4Label] = nowQuestions[3];
+
+                    break;
+
+                case 2:
+                    answer1Label.Text = nowQuestions[2].Mean;
+                    answer2Label.Text = nowQuestions[1].Mean;
+                    answer3Label.Text = nowQuestions[0].Mean;
+                    answer4Label.Text = nowQuestions[3].Mean;
+
+                    dict[answer1Label] = nowQuestions[2];
+                    dict[answer2Label] = nowQuestions[1];
+                    dict[answer3Label] = nowQuestions[0];
+                    dict[answer4Label] = nowQuestions[3];
+
+                    break;
+
+                case 3:
+                    answer1Label.Text = nowQuestions[3].Mean;
+                    answer2Label.Text = nowQuestions[1].Mean;
+                    answer3Label.Text = nowQuestions[2].Mean;
+                    answer4Label.Text = nowQuestions[0].Mean;
+
+                    dict[answer1Label] = nowQuestions[3];
+                    dict[answer2Label] = nowQuestions[1];
+                    dict[answer3Label] = nowQuestions[2];
+                    dict[answer4Label] = nowQuestions[0];
+
+                    break;
+            }
+        }
+
+        private void ShowAnswer()
+        {
+            // 回答表示中は何もしない
+            if (nextButton.Visible)
+            {
+                return;
+            }
+
+            // 正解の場合、文字色を変える
+            if (answer1Label.Text == nowQuestions[0].Mean)
+            {
+                answer1Label.ForeColor = Color.Red;
+            }
+            else if (answer2Label.Text == nowQuestions[0].Mean)
+            {
+                answer2Label.ForeColor = Color.Red;
+            }
+            else if (answer3Label.Text == nowQuestions[0].Mean)
+            {
+                answer3Label.ForeColor = Color.Red;
+            }
+            else
+            {
+                answer4Label.ForeColor = Color.Red;
+            }
+
+            answer1Label.Text = dict[answer1Label].Word + "..." + dict[answer1Label].Mean;
+            answer2Label.Text = dict[answer2Label].Word + "..." + dict[answer2Label].Mean;
+            answer3Label.Text = dict[answer3Label].Word + "..." + dict[answer3Label].Mean;
+            answer4Label.Text = dict[answer4Label].Word + "..." + dict[answer4Label].Mean;
+
+            nextButton.Visible = true;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            GetQuestion();
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            nextButton.Visible = false;
+
+            answer1Label.ForeColor = Color.Black;
+            answer2Label.ForeColor = Color.Black;
+            answer3Label.ForeColor = Color.Black;
+            answer4Label.ForeColor = Color.Black;
+
+            GetQuestion();
+        }
+
+        private void answer1Label_Click(object sender, EventArgs e)
+        {
+            ShowAnswer();
+        }
+
+        private void answer2Label_Click(object sender, EventArgs e)
+        {
+            ShowAnswer();
+        }
+
+        private void answer3Label_Click(object sender, EventArgs e)
+        {
+            ShowAnswer();
+        }
+
+        private void answer4Label_Click(object sender, EventArgs e)
+        {
+            ShowAnswer();
         }
     }
 }
